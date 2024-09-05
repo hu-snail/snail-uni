880da729c541002e09d9e0146fea46e33bd9e15f
hu-snail <1217437592@qq.com>
2024-09-05 09:49:27 +0800

    fix: 完善js模版解析

--- template/src/router/white-list.ts
+++ template/src/router/white-list.ts
@@ -5,13 +5,13 @@ const { VITE_OPNE_NO_LOGIN } = import.meta.env;
  * 请根据自己的业务进行调整
  */
 /** 免登录白名单（匹配路由 path） */
-const whiteListByPath: string[] = ['/sub-pages/login/index'];
+const whiteListByPath<% if (useTs) { %>: string[]<% } %> = ['/sub-pages/login/index'];
 
 /** 需要登录的白名单 匹配路由 path） */
-const loginList: string[] = ['/sub-pages/detail/index'];
+const loginList<% if (useTs) { %>: string[]<% } %> = ['/sub-pages/detail/index'];
 
 /** 判断是否在白名单 */
-export const isWhiteList = (to: any) => {
+export const isWhiteList = (to<% if (useTs) { %>: any<% } %>) => {
   if (JSON.parse(VITE_OPNE_NO_LOGIN)) return whiteListByPath.indexOf(to) !== -1;
   else {
     return loginList.indexOf(to) === -1;
